0dba3adf67f71bd586138db88dea56a11016ecbb
AllanSantanna <allanxlink@hotmail.com>
2024-07-03 20:29:23 -0300

    font configuration

---
+++ src/app/document.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @next/next/no-page-custom-font */
+/* eslint-disable @next/next/no-document-import-in-page */
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Mina:wght@700&family=Roboto:wght@400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
