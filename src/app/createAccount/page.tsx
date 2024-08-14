066e99a497d0aa584aef95597466c44435ab86ee
AllanSantanna <allanxlink@hotmail.com>
2024-08-14 20:12:10 -0300

    feat: create page createAccount

---
+++ src/app/createAccount/page.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import React from 'react';
+import CreateAccountTemplate from '../../components/templates/createAccount';
+
+export default function CreateAccount() {
+  return <CreateAccountTemplate />;
+}
