391ce5547b5428de2f4c465aac877a768f1644a5
AllanSantanna <allanxlink@hotmail.com>
2024-09-25 13:02:28 -0300

    feat: retur to login page

--- src/app/createAccount/page.tsx
+++ src/app/createAccount/page.tsx
@@ -1,10 +1,13 @@
 'use client'
 
 import React from 'react'
+import { useRouter } from 'next/navigation'
 import CreateAccountTemplate from '../../components/templates/createAccount'
 import { createUser } from '../../api/create-user'
 
 export default function CreateAccount() {
+  const router = useRouter()
+
   const handleCreateUser = async (formData: {
     email: string
     password: string
@@ -12,6 +15,8 @@ export default function CreateAccount() {
   }) => {
     try {
       const response = await createUser(formData)
+      window.alert('Conta criada com sucesso!')
+      router.push('/login')
       console.log('Usuário criado com sucesso:', response)
     } catch (error) {
       console.error('Erro ao criar usuário:', error)
